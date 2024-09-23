9d9d2480565a192a9e6dd1fd2d48a26c7bf0e462
zaga <163298261+zaga001@users.noreply.github.com>
2024-09-23 10:11:08 +0700

    Add operate function and operation variables

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ const add = (num1, num2) => num1 + num2;
 const subtract = (num1, num2) => num1 - num2;
 const multiply = (num1, num2) => num1 * num2;
 const divide = (num1, num2) => num1 / num2;
+
+let num1, num2, operator;
+
+function operate(num1, operator, num2) {
+    if (operator === add) return add(num1, num2);
+    if (operator === subtract) return subtract(num1, num2);
+    if (operator === multiply) return multiply(num1, num2);
+    if (operator === divide) return divide(num1, num2);
+}
